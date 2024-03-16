7c4683dcce82b081280388c803f25a4b2d85ef71
ElvinLin <ssfvessfve@163.com>
2024-03-16 18:06:56 +0800

    add change

--- main.tf
+++ main.tf
@@ -1,27 +1,61 @@
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = ""
-  secret_key = ""
+
 }
 
+variable "vpc_cidr_block" {
+  description= "vpc cidr block"
+}
+variable "cidr_blocks" {
+    description = "environment used"
+  type= list(object({
+    value = string
+    name = string
+  }))
+}
 
 resource "aws_vpc" "development-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_blocks[0].value
+  tags = {
+    Name= var.cidr_blocks[0].name
+  }
+}
 
+variable "subnet_cidr_block" {
+    description= "subnet cidr block"
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
-  cidr_block = "10.0.10.0/24"
+  cidr_block = var.subnet_cidr_block
   availability_zone = "ap-southeast-1a"
+  tags = {
+    Name= "subnet-1-dev"
+    vpn-env= "dev"
+  }
 }
 
 data "aws_vpc" "existing_vpc" {
   default = true
 }
 
+
+
 resource "aws_subnet" "dev-subnet-2" {
   vpc_id = data.aws_vpc.existing_vpc.id
   cidr_block = "172.31.48.0/20"
   availability_zone = "ap-southeast-1a"
+  tags = {
+    Name= "subnet-2-default"
+  }
+}
+
+output "dev-vpc-id" {
+  value = aws_vpc.development-vpc.id
+}
+output "dev-subnet1-id" {
+  value = aws_subnet.dev-subnet-1.id
+}
+output "dev-subnet2-id" {
+  value = aws_subnet.dev-subnet-2.id
 }
---
+++ terraform-dev.tfvars
@@ -0,0 +1,8 @@
+subnet_cidr_block = "10.0.40.0/24"
+vpc_cidr_block = "10.0.0.0/16"
+environment = "development"
+
+cidr_blocks = [
+  {value:"10.0.40.0/24", name:"subnet_cidr_block"},
+  {value:"10.0.0.0/16", name:"vpc_cidr_block"}
+]
